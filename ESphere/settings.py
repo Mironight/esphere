699727545106dd3fc30840405e31100d20e32d30
Mike Röwekamp <mike.develop@outlook.de>
2021-08-15 17:22:53 +0200

    django set auto pk field

--- ESphere/settings.py
+++ ESphere/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
